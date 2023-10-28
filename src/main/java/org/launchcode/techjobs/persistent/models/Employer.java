c26109b82ef3ff4abe60ee48def859f92b06db98
erinmuzughi <erinmuzughi@gmail.com>
2023-10-27 22:08:35 -0500

    halfway through task 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,28 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotNull(message = "Location is required")
+    @Size(min = 3, max = 50, message = "Location name must be between 3 and 50 characters")
+    private String location;
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Skill extends AbstractEntity {
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
+@Entity
+public class Skill extends AbstractEntity {
+    @NotNull(message = "Description is required")
+    @Size(min = 25, max = 500, message = "Skill description must be between 25 and 500 characters")
+    private String description;
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill(){}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
