d314337c57d477136290d818e9f8b58d4408f4b4
erinmuzughi <erinmuzughi@gmail.com>
2023-10-27 21:42:37 -0500

    finished part 1

--- queries.sql
+++ queries.sql
@@ -1,4 +1,8 @@
 --Part 1
+-- column name: id | data type: int
+-- column name: employer |  data type: varchar(255)
+-- column name: name |  data type: varchar(255)
+-- column name: skills |  data type: varchar(255)
 
 --Part 2
 
