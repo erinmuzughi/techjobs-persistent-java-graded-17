6cea09a277e9bbdb54bc45170101653cd515e8d3
erinmuzughi <erinmuzughi@gmail.com>
2023-11-07 18:47:51 -0600

    fixed failing SQL test

--- queries.sql
+++ queries.sql
@@ -12,8 +12,8 @@ WHERE location = "St. Louis City";
 DROP TABLE job;
 
 --Part 4
-SELECT name
+SELECT *
 FROM skill
-LEFT JOIN job_skills ON (skill.id = job_skills.skills_id)
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
 WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
